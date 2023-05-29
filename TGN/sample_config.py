2b7e061c985512313b37954cc5530ccfe89dd8be
mephisto-of-hell <125276079+mephisto-of-hell@users.noreply.github.com>
2023-05-29 15:18:23 +0530

    Update sample_config.py

--- TGN/sample_config.py
+++ TGN/sample_config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = ""
+    API_ID = 23765362  # integer value, dont use ""
+    API_HASH = "43e45c913470ed97c809b5dac9ab791c"
+    TOKEN = "BOT_TOKEN", "6046334142:AAG_mQp8h8iPzRa7j5P8A2sXBY4QfNkrM68"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 5023815012  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "god_of_illusion"
     SUPPORT_CHAT = ""  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001952906636
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001952906636
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
